dd29f0fef7917af73cd72f753277a379ebaacf7e
hyptos <hyptos974@gmail.com>
2018-09-30 23:22:59 +0200

    redirection vers admin

--- app/src/Controller/ProfileController.php
+++ app/src/Controller/ProfileController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 /** @Route("/profile") */
-class ProfileController extends Controller {
+class ProfileController extends Controller
+{
 
     /**
      * @Route("/profile")
      */
-    public function index() {
-        return $this->render('profile/index.html.twig', ['mainNavMember'=>true, 'title'=>'Espace Membre']);
+    public function index()
+    {
+
+        return $this->render('profile/index.html.twig', ['mainNavMember' => true, 'title' => 'Espace Membre']);
     }
 
 }
