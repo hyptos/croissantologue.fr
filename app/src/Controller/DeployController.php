ab28a225afbee03ea1ae25f9f40ea359cf89f062
hyptos <hyptos974@gmail.com>
2018-10-01 13:51:48 +0200

    ajout du deploy

---
+++ app/src/Controller/DeployController.php
@@ -0,0 +1,42 @@
+<?php
+
+// src/Controller/DeployController.php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DeployController extends AbstractController
+{
+    /**
+    * @Route("/deploy", name="deploy")
+    */
+    public function index()
+    {
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git reset --hard HEAD',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+    
+	return new Response(
+            '<html><body> '.$output.'</body></html>'
+        );
+
+     }
+}
