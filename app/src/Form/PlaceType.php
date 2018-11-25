809fd10a50d38d98807cb9ee2a2e639844e188e3
hyptos <hyptos974@gmail.com>
2018-11-25 21:38:31 +0100

    place form

--- app/src/Form/PlaceType.php
+++ app/src/Form/PlaceType.php
@@ -4,11 +4,23 @@
 namespace App\Form;
 
 use App\Entity\Place;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PlaceType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('address', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Envoyer'))
+        ;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
