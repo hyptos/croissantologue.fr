11986dfbe60961fa1ab3e5dc9baaad3c4fb6d928
hyptos <hyptos974@gmail.com>
2018-11-30 21:37:12 +0100

    place form

--- app/src/Form/PlaceType.php
+++ app/src/Form/PlaceType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 use App\Entity\Place;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
